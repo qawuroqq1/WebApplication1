f01cb2a3281b8478abfa08b8debe5abe08409c5b
Данияр Макашев <danik.makashev@bk.ru>
2026-01-12 17:16:38 +0500

    old version

--- WebApplication1/Controllers/OrdersController.cs
+++ WebApplication1/Controllers/OrdersController.cs
@@ -1,38 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
 using WebApplication1.Services;
 
-namespace WebApplication1.Controllers
+public class OrdersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrdersController : ControllerBase
+ 
+
+    public OrdersController(OrderService service)
     {
-        private readonly OrderService _service; 
-
-        public OrdersController(OrderService service)
-        {
-            _service = service;
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(Guid id)
-        {
-            var order = await _service.GetByIdAsync(id);
-            return order == null ? NotFound() : Ok(order);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(Guid id, Order order)
-        {
-            if (id != order.Id) return BadRequest();
-            return await _service.UpdateAsync(order) ? NoContent() : NotFound();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            return await _service.DeleteAsync(id) ? NoContent() : NotFound();
-        }
+        _service = service;
     }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id) => Ok(await _service.GetByIdAsync(id));
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id) => await _service.DeleteAsync(id) ? NoContent() : NotFound();
 }
